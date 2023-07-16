101b6f1ac36b8a52be582223d7873d0e1b4e8a39
VorobyovDV <vorobyovdv@gnivc.ru>
2023-07-16 22:04:50 +0300

    основной функционал

---
+++ DvEng/ChatGpt_DvEng/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatGpt_DvEng
+{
+    public partial class MainForm : Form
+    {
+        private List<string> words;
+        private List<string> translations;
+        private List<Button> buttons;
+        private Random random;
+        private int guessedCount;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            words = new List<string>
+            {
+                "apple", "banana", "cat", "dog", "elephant",
+                "яблоко", "банан", "кошка", "собака", "слон"
+            };
+
+            translations = new List<string>();
+            buttons = new List<Button>();
+            random = new Random();
+            guessedCount = 0;
+
+            InitializeButtons();
+            ShowRandomWords();
+        }
+
+        private void InitializeButtons()
+        {
+            buttons.Add(button1);
+            buttons.Add(button2);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button5);
+            buttons.Add(button6);
+            buttons.Add(button7);
+            buttons.Add(button8);
+            buttons.Add(button9);
+            buttons.Add(button10);
+        }
+
+        private void ShowRandomWords()
+        {
+            var shuffledWords = words.OrderBy(x => random.Next()).ToList();
+            var shuffledTranslations = shuffledWords.Skip(5).OrderBy(x => random.Next()).ToList();
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (i < 5)
+                {
+                    buttons[i].Text = shuffledWords[i];
+                    buttons[i].Tag = shuffledTranslations[i];
+                }
+                else
+                {
+                    buttons[i].Text = shuffledTranslations[i - 5];
+                    buttons[i].Tag = shuffledWords[i - 5];
+                }
+            }
+        }
+
+        private void CheckGuess(Button button)
+        {
+            if (button.Tag.ToString() == button.Text)
+            {
+                button.Enabled = false;
+                guessedCount++;
+            }
+
+            if (guessedCount == 3)
+            {
+                guessedCount = 0;
+                RefreshButtons();
+            }
+        }
+
+        private void RefreshButtons()
+        {
+            var remainingWords = words.Except(buttons.Select(b => b.Tag.ToString())).ToList();
+            var remainingTranslations = remainingWords.Skip(5).ToList();
+            remainingWords = remainingWords.Take(5).ToList();
+
+            var shuffledWords = remainingWords.OrderBy(x => random.Next()).ToList();
+            var shuffledTranslations = remainingTranslations.OrderBy(x => random.Next()).ToList();
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (i < 5)
+                {
+                    if (!buttons[i].Enabled)
+                    {
+                        buttons[i].Enabled = true;
+                        buttons[i].Text = shuffledWords[i];
+                        buttons[i].Tag = shuffledTranslations[i];
+                    }
+                }
+                else
+                {
+                    if (!buttons[i].Enabled)
+                    {
+                        buttons[i].Enabled = true;
+                        buttons[i].Text = shuffledTranslations[i - 5];
+                        buttons[i].Tag = shuffledWords[i - 5];
+                    }
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            CheckGuess(button);
+        }
+    }
+}
---
+++ DvEng/DvEng/Models/Model.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DvEng.Models
+{
+    internal class Model
+    {
+        private string[] _values = new string[5];
+        private string[] _translates = new string[5];
+
+        private Random random = new Random();
+
+        private List<Word> _words;
+        private Word[] _currentWords = new Word[5];
+
+        public string[] Values { get => _values; }
+        public string[] Translates { get => _translates; }
+
+        public void SetWords(List<Word> words)
+        { _words = words; }
+
+        public Model GetWords()
+        {
+       
+
+            GenerateWords();
+
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if (string.IsNullOrEmpty(_values[i]))
+                {
+                    _values[i] = _currentWords[i].WordValue;
+                    
+                    var translateEmptyIndex = _translates.Select(x=>string.IsNullOrEmpty(x)).ToList();
+
+                    bool isGood = false;
+                    int randomTranslateEmptyIndex = 0;
+
+                    while(isGood == false)
+                    {
+                        randomTranslateEmptyIndex = random.Next(translateEmptyIndex.Count);
+
+                        if (translateEmptyIndex[randomTranslateEmptyIndex] == true)
+                            isGood = true;
+                    }
+
+                    _translates[randomTranslateEmptyIndex] = _currentWords[i].Translate;
+                }
+            }
+
+            return this;
+        }
+
+        public bool CheckWords(int indexC1, int indexC2)
+        {
+            bool result = false;
+
+            string value = _values[indexC1];
+            string translate = _translates[indexC2];
+
+            Word word = _words.Where(x=>x.WordValue == value).FirstOrDefault();
+
+            if(word.WordValue == value && word.Translate == translate)
+            {
+                result = true;
+
+                int indexCurrentWords = Array.IndexOf(_currentWords, word);
+                _currentWords[indexCurrentWords] = null;
+                _values[indexC1] = string.Empty;
+                _translates[indexC2] = string.Empty;
+                
+                //for (int i = 0; i < currentWords.Length; i++)
+                //{
+                //    currentWords[i] = null;
+                //}
+                //for (int i = 0; i < _values.Length; i++)
+                //{
+                //    _values[i] = null;
+                //    _translates[i] = null;
+                //}
+
+
+            }
+
+            return result;
+        }
+
+        private void GenerateWords()
+        {
+            for (int i = 0; i < _currentWords.Length; i++)
+            {
+                if (_currentWords[i] == null)
+                {
+                    _currentWords[i] = GetRandomWord();
+                }
+            }
+        }
+
+        private Word GetRandomWord()
+        {
+            int randomWordIndex = random.Next(_words.Count);
+            return _words[randomWordIndex];
+        }
+    }
+}
